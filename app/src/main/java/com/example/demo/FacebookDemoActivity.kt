9871bd1d1fe3d92b31d1bad9ee14b4b678228087
ixidev <feedback.mrzero@gmail.com>
2021-02-01 18:09:11 +0100

    - Add  Facebook Interstitial demo Ad

--- app/src/main/java/com/example/demo/FacebookDemoActivity.kt
+++ app/src/main/java/com/example/demo/FacebookDemoActivity.kt
@@ -1,22 +1,33 @@
 package com.example.demo
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.facebook.ads.AdSettings
 import com.facebook.ads.AudienceNetworkAds
 import com.ixidev.adstoolkit.core.IBannerAd
+import com.ixidev.adstoolkit.core.IInterstitialAd
 import com.ixidev.adstoolkit.facebook.SimpleFacebookBannerAd
+import com.ixidev.adstoolkit.facebook.SimpleFacebookInterstitialAd
 
 class FacebookDemoActivity : AppCompatActivity() {
 
     private lateinit var banner: IBannerAd
+    private lateinit var interstitialAd: IInterstitialAd
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_facebook_demo)
         AudienceNetworkAds.initialize(this)
         AdSettings.addTestDevice("5211dd89-8793-4516-a5e7-8583bc4aaedf")
         initBanner()
+        initFullScreenAd()
+    }
+
+    private fun initFullScreenAd() {
+        interstitialAd = SimpleFacebookInterstitialAd()
+        interstitialAd.load(this, "1444710325874143_1444718842539958")
     }
 
     private fun initBanner() {
@@ -27,5 +38,8 @@ class FacebookDemoActivity : AppCompatActivity() {
 
     @Suppress("UNUSED_PARAMETER")
     fun showFullScreenAd(view: View) {
+        interstitialAd.show {
+            Toast.makeText(this, "Ad closed or not loaded", Toast.LENGTH_SHORT).show()
+        }
     }
 }
