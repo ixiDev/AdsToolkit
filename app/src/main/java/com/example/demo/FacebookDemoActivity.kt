aa88a75810f28caf7845a34b2d50416f9fd38789
ixidev <feedback.mrzero@gmail.com>
2021-02-01 18:03:04 +0100

    Add facebbook banner ad  demo

--- app/src/main/java/com/example/demo/FacebookDemoActivity.kt
+++ app/src/main/java/com/example/demo/FacebookDemoActivity.kt
@@ -2,10 +2,26 @@ package com.example.demo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.ixidev.adstoolkit.core.IBannerAd
+import com.ixidev.adstoolkit.facebook.SimpleFacebookBannerAd
 
 class FacebookDemoActivity : AppCompatActivity() {
+
+    private lateinit var banner: IBannerAd
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_facebook_demo)
+        initBanner()
+    }
+
+    private fun initBanner() {
+        banner = SimpleFacebookBannerAd()
+        banner.load(this, "1444710325874143_1444712852540557")
+        banner.render(findViewById(R.id.fb_simple_banner))
+    }
+
+    @Suppress("UNUSED_PARAMETER")
+    fun showFullScreenAd(view: View) {
     }
 }
